da0bc600438cd52236bddc6906e0ccc2f9313c2e
barnrang <>
2018-04-15 00:35:59 +0900

    fixed queue

--- fast_queue.py
+++ fast_queue.py
@@ -5,23 +5,29 @@ eps = 10e-8
 
 class fast_queue(keras.callbacks.Callback):
     def __init__(self, size):
-        self.data = [None] * size
-        self.sam_loss = [eps] * size
-        self.indice = list(range(0, size))
+        self.data = [None] * (size + 1)
+        self.sam_loss = [0] * (size + 1)
+        self.indice = list(range(0, size + 1))
         
         # state index (training)
         self.idx = 0
 
 
-        self.size = size
+        self.size = size + 1
         self.start = 0
         self.end = 0
+
+    def __get__(self, obj, objtype):
+        if self.start <= self.end:
+            print([self[x] for x in range(self.start, self.end)])
+        else:
+            print([self[x] for x in range(self.start, self.end + self.size)])
 
     def __len__(self):
         if self.start <= self.end:
             return self.end - self.start
         else:
-            return self.start + self.end - 1
+            return self.size + self.end - self.start
 
     def __getitem__(self, idx):
         return self.data[(self.start + idx) % self.size]
@@ -40,10 +46,19 @@ class fast_queue(keras.callbacks.Callback):
 
     def append(self, item):
         self.data[self.end] = item
-        self.sam_loss[self.end] = 0
+        self.sam_loss[self.end] = eps
         self.end = (self.end + 1) % self.size
 
         if self.start == self.end:
+            self.sam_loss[self.start] = 0
             self.start = (self.start + 1) % self.size
 
-    
+if __name__ == '__main__':
+    test = fast_queue(10)
+    for i in range(6):
+        test.append(i)
+    for i in range(5):
+        test.append(i)
+    print(test)
+    print(len(test))
+    print((test.start, test.end))  
